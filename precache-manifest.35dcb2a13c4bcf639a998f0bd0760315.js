2d9f5d67fecf3dae003b713c63fac94367b1aec1
shpakweb <shpakweb@gmail.com>
2019-05-07 23:01:52 +0300

    Updates

---
+++ precache-manifest.35dcb2a13c4bcf639a998f0bd0760315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f2e4a5cc26e96e75e3fe8e30abcd6c",
+    "url": "/movieme/index.html"
+  },
+  {
+    "revision": "02d455a2e6939e72efa4",
+    "url": "/movieme/static/css/main.6258871c.chunk.css"
+  },
+  {
+    "revision": "8b66700dab28c3b3a40f",
+    "url": "/movieme/static/js/2.a9611dfb.chunk.js"
+  },
+  {
+    "revision": "02d455a2e6939e72efa4",
+    "url": "/movieme/static/js/main.479af4e0.chunk.js"
+  },
+  {
+    "revision": "f8ffd81700573edc9a03",
+    "url": "/movieme/static/js/runtime~main.0abcb08d.js"
+  },
+  {
+    "revision": "cb90c5bda3dbe4e16f7607c0c5213f3a",
+    "url": "/movieme/static/media/logo.cb90c5bd.svg"
+  }
+]);
